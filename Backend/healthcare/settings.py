18162b847e27e29c927634c16c58e36ca7fbf61b
SBK-DS <89160790+SBK-DS@users.noreply.github.com>
2024-06-03 15:02:47 +0500

    Update settings.py

--- Backend/healthcare/settings.py
+++ Backend/healthcare/settings.py
@@ -9,6 +9,9 @@ from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = get_random_secret_key()
 
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEBUG = True
 
