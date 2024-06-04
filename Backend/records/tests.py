fa4591609c13dfa5bd56caf74fddebfc4ed9e81c
ahmedlaal <ahmedhamzakhaliq@gmail.com>
2024-06-04 05:14:57 -0700

    Test Cases Added

--- Backend/records/tests.py
+++ Backend/records/tests.py
@@ -1,3 +1,104 @@
-from django.test import TestCase
+# records/tests.py
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from records.models import BasicDetailsM, DailyRecordsNameM, DailyRecordsValueM
 
-# Create your tests here.
+
+class BasicDetailsTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_basic_details(self):
+        url = '/besicdetails/'
+        data = {
+            "user": self.user.id,
+            "date_of_diagnosis": "2023-01-01T00:00:00Z",
+            "admitted_in_hospital": "2023-01-01T00:00:00Z",
+            "covid_test_done": "Yes",
+            "covid_test_type": "PCR",
+            "covid_test_report": "Negative"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(BasicDetailsM.objects.count(), 1)
+        self.assertEqual(BasicDetailsM.objects.get().covid_test_done, 'Yes')
+
+    def test_get_basic_details(self):
+        BasicDetailsM.objects.create(user=self.user, covid_test_done='Yes', covid_test_type='PCR', covid_test_report='Negative')
+        url = f'/besicdetails/{self.user.id}/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['covid_test_done'], 'Yes')
+
+    def test_update_basic_details(self):
+        basic_details = BasicDetailsM.objects.create(user=self.user, covid_test_done='Yes', covid_test_type='PCR', covid_test_report='Negative')
+        url = f'/besicdetails/{self.user.id}/'
+        data = {
+            "user": self.user.id,
+            "covid_test_done": "No",
+            "covid_test_type": "Antigen",
+            "covid_test_report": "Positive"
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        basic_details.refresh_from_db()
+        self.assertEqual(basic_details.covid_test_done, 'No')
+
+    def test_delete_basic_details(self):
+        basic_details = BasicDetailsM.objects.create(user=self.user, covid_test_done='Yes', covid_test_type='PCR', covid_test_report='Negative')
+        url = f'/besicdetails/{self.user.id}/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(BasicDetailsM.objects.count(), 0)
+
+
+class DailyRecordsTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.record_name = DailyRecordsNameM.objects.create(recordname="Temperature")
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_daily_record(self):
+        url = '/dailyrecords/'
+        data = {
+            "user": self.user.id,
+            "recordname": self.record_name.id,
+            "value": 98,
+            "datetime": "2023-01-01T00:00:00Z"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(DailyRecordsValueM.objects.count(), 1)
+        self.assertEqual(DailyRecordsValueM.objects.get().value, 98)
+
+    def test_get_daily_records(self):
+        DailyRecordsValueM.objects.create(user=self.user, recordname=self.record_name, value=98)
+        url = f'/dailyrecords/{self.user.id}/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['value'], 98)
+
+    def test_update_daily_record(self):
+        daily_record = DailyRecordsValueM.objects.create(user=self.user, recordname=self.record_name, value=98)
+        url = f'/dailyrecords/{daily_record.id}/'
+        data = {
+            "user": self.user.id,
+            "recordname": self.record_name.id,
+            "value": 99,
+            "datetime": "2023-01-01T00:00:00Z"
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        daily_record.refresh_from_db()
+        self.assertEqual(daily_record.value, 99)
+
+    def test_delete_daily_record(self):
+        daily_record = DailyRecordsValueM.objects.create(user=self.user, recordname=self.record_name, value=98)
+        url = f'/dailyrecords/{self.user.id}/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(DailyRecordsValueM.objects.count(), 0)
--- Backend/records/views.py
+++ Backend/records/views.py
@@ -1,6 +1,6 @@
+# records/views.py
 from django.shortcuts import render
-
-from rest_framework import views,response,status,viewsets,permissions
+from rest_framework import views, response, status, viewsets, permissions
 from . import models as RecordsModel
 from . import serializers as RecordsModelSerializer
 from django.shortcuts import get_object_or_404
@@ -9,89 +9,90 @@ from rest_framework import authentication, permissions
 
 class BasicDetailsC(viewsets.ViewSet):
     permission_classes = (permissions.IsAuthenticated,)
-    serializer_class=RecordsModelSerializer.BasicDetailsMS
-    queryset=RecordsModel.BasicDetailsM.objects.all()
+    serializer_class = RecordsModelSerializer.BasicDetailsMS
+    queryset = RecordsModel.BasicDetailsM.objects.all()
 
-    def list(self,request):
+    def list(self, request):
         print(request.user)
-        serializer=self.serializer_class(self.queryset,many=True)
+        serializer = self.serializer_class(self.queryset, many=True)
         return response.Response(serializer.data)
 
-    def retrieve(self,request,pk=None):
-        blog=get_object_or_404(self.queryset,user_id=pk)
-        print('*'*20)
+    def retrieve(self, request, pk=None):
+        blog = get_object_or_404(self.queryset, user_id=pk)
+        print('*' * 20)
         print(blog)
-        serializer=self.serializer_class(blog)
+        serializer = self.serializer_class(blog)
         return response.Response(serializer.data)
 
-    def create(self,request):
-        data=request.data
-        serializer=self.serializer_class(data=data)
+    def create(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)  # Set status to 201
+
+    def update(self, request, pk=None):
+        blog = get_object_or_404(self.queryset, user_id=pk)
+        data = request.data
+        serializer = self.serializer_class(blog, data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return response.Response(serializer.data)
 
-    def update(self,request,pk=None):
-        blog=get_object_or_404(self.queryset,user_id=pk)
-        data=request.data
-        serializer=self.serializer_class(blog,data)
+    def partial_update(self, request, pk=None):
+        blog = get_object_or_404(self.queryset, user_id=pk)
+        data = request.data
+        serializer = self.serializer_class(blog, data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return response.Response(serializer.data)
+        return response.Response(serializer.data, status=200)
 
-    def partial_update(self,request,pk=None):
-        blog=get_object_or_404(self.queryset,user_id=pk)
-        data=request.data
-        serializer=self.serializer_class(blog,data,partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return response.Response(serializer.data,status=200)
-
-    def destroy(self,request,pk=None):
-        instances=get_object_or_404(self.queryset,pk=pk)
+    def destroy(self, request, pk=None):
+        instances = get_object_or_404(self.queryset, pk=pk)
         instances.delete()
         return response.Response(status=200)
 
+
 class DailyRecordsC(viewsets.ViewSet):
     permission_classes = (permissions.IsAuthenticated,)
-    serializer_class=RecordsModelSerializer.DailyRecordsValueMS
-    queryset=RecordsModel.DailyRecordsValueM.objects.all()
+    serializer_class = RecordsModelSerializer.DailyRecordsValueMS
+    queryset = RecordsModel.DailyRecordsValueM.objects.all()
 
-    def list(self,request):
+    def list(self, request):
         print(request.user)
-        serializer=self.serializer_class(self.queryset,many=True)
+        serializer = self.serializer_class(self.queryset, many=True)
         return response.Response(serializer.data)
 
-    def retrieve(self,request,pk=None,):
-        blog=RecordsModel.DailyRecordsValueM.objects.filter(user_id=pk)
-        print('*'*20)
+    def retrieve(self, request, pk=None,):
+        blog = RecordsModel.DailyRecordsValueM.objects.filter(user_id=pk)
+        print('*' * 20)
         print(blog)
         return response.Response(list(blog.values()))
 
-    def create(self,request):
-        data=request.data
-        serializer=self.serializer_class(data=data)
+    def create(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)  # Set status to 201
+
+    def update(self, request, pk=None):
+        blog = get_object_or_404(self.queryset, pk=pk)
+        data = request.data
+        serializer = self.serializer_class(blog, data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return response.Response(serializer.data)
 
-    def update(self,request,pk=None):
-        blog=get_object_or_404(self.queryset,pk=pk)
-        data=request.data
-        serializer=self.serializer_class(blog,data)
+    def partial_update(self, request, pk=None):
+        blog = get_object_or_404(self.queryset, pk=pk)
+        data = request.data
+        serializer = self.serializer_class(blog, data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return response.Response(serializer.data)
+        return response.Response(serializer.data, status=200)
 
-    def partial_update(self,request,pk=None):
-        blog=get_object_or_404(self.queryset,pk=pk)
-        data=request.data
-        serializer=self.serializer_class(blog,data,partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return response.Response(serializer.data,status=200)
-
-    def destroy(self,request,pk=None):
-        instances=RecordsModel.DailyRecordsValueM.objects.filter(user_id=pk)
+    def destroy(self, request, pk=None):
+        instances = RecordsModel.DailyRecordsValueM.objects.filter(user_id=pk)
         instances.delete()
         return response.Response(status=200)
